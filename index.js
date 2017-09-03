cd62617da7afd0c1731112beaa45ebc5cbaf4673
Ashley Casey <acasey.dev@gmail.com>
2017-09-03 15:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) {
+    return "I can\'t hear you!";
+  }
+  elseif (string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  elseif (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
